fe43b6bfc1aac5799ddb08d9410cbdf10ffef7c7
Sameh Elsayed <ssayed7190@gmail.com>
2021-06-05 18:23:14 +0400

    Cache the Handsets service response
    the response should be cached because the data is static, and this cache will increase the performance

--- mobile-search-api/src/main/java/com/hyke/mobilesearch/MobileSearchApplication.java
+++ mobile-search-api/src/main/java/com/hyke/mobilesearch/MobileSearchApplication.java
@@ -2,8 +2,10 @@ package com.hyke.mobilesearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MobileSearchApplication {
 
 	public static void main(String[] args) {
--- mobile-search-api/src/main/java/com/hyke/mobilesearch/service/MobileService.java
+++ mobile-search-api/src/main/java/com/hyke/mobilesearch/service/MobileService.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.hyke.mobilesearch.dao.MobileDAO;
+import com.hyke.mobilesearch.handler.CacheHandler;
 import com.hyke.mobilesearch.model.Handset;
 import com.hyke.mobilesearch.predicate.HandsetPredicateGenerator;
 
@@ -19,16 +19,16 @@ public class MobileService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MobileService.class);
 
 	@Autowired
-	private MobileDAO mobileDAO;
+	private CacheHandler cacheHandler;
 
 	public List<Handset> search(Map<String, String> criteria) {
 		LOGGER.info("search - criteria {}", criteria);
 
-		List<Handset> mobileDevices = mobileDAO.getAllDevices();
+		List<Handset> mobileDevices = cacheHandler.getAllHandsets();
 
 		if (mobileDevices != null && !(mobileDevices.isEmpty())) {
 			List<Predicate<Handset>> predicates = HandsetPredicateGenerator.generate(criteria);
-			
+
 			if (predicates != null && !(predicates.isEmpty())) {
 				mobileDevices = mobileDevices.stream().filter(predicates.stream().reduce(p -> true, Predicate::and))
 						.collect(Collectors.<Handset>toList());
